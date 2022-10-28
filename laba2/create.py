267db580ac62a4ba17bb701301356b44022af28f
Pi-Jey <62254896+Pi-Jey@users.noreply.github.com>
2022-10-28 23:07:56 +0300

    Second Lab

--- laba2/create.py
+++ laba2/create.py
@@ -6,8 +6,6 @@ class Create(e.Element):
         super().__init__(delay)
 
     def out_act(self):
-        # виконуємо збільшення лічильника кількості
         super().out_act()
-        # встановлюємо коли пристрій буде вільним
         self.t_next[0] = self.t_curr + self.get_delay()
         self.next_element[0].in_act()
